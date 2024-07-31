1ecbbfc342363fa799518f00abac066c907ae36d
Alex <aleksandrbasanets2012@gmail.com>
2024-07-31 14:58:03 +0300

    Implement CI on the project. Add SonarQube. Fix errors v1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-/*
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp.library) apply false
+    //id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "5.1.0.4882"
 
-}*/
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "test-test-test2_test-project")
+        property("sonar.organization", "test-test-test2")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
 buildscript {
     repositories {
         google()
@@ -15,5 +26,6 @@ buildscript {
     }
 
     dependencies {
+        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:5.1.0.4882")
     }
 }
