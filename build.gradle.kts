cdd095dc85833586fead00af43bc7e56731fe073
Alex <aleksandrbasanets2012@gmail.com>
2024-08-01 13:25:32 +0300

    Implement CI on the project. Remove SonarCloud for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp.library) apply false
-    //id("org.sonarqube") version "4.4.1.3373"
-    id("org.sonarqube") version "5.1.0.4882"
+    //id("org.sonarqube") version "5.1.0.4882"
 
 }
 
-sonar {
+/*sonar {
     properties {
         property("sonar.projectKey", "alex-basanets-org_book-trails")
         property("sonar.organization", "alex-basanets-org")
         property("sonar.host.url", "https://sonarcloud.io")
     }
-}
+}*/
 
 buildscript {
     repositories {
@@ -26,6 +24,6 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:5.1.0.4882")
+        //classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:5.1.0.4882")
     }
 }
